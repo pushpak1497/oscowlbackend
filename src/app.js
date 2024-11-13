572f1bc02ffb38b58a2f96f1cd31c8c6dfbc90de
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-11-13 16:46:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["http://localhost:3000", "https://oscowlfrontend.vercel.app"],
   })
 );
 
